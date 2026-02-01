7169bb8913ba3b537b0cd4b4eca9738948567e3e
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-02-01 11:34:30 +0100

    feat(map): add 'find closest' functionality and car selection for plugging in

--- src/components/ShowChargingStationModal.vue
+++ src/components/ShowChargingStationModal.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import FormField from '@/components/FormField.vue';
+import type { Car } from '@/types/car';
 import type { ShowableChargingStation } from '@/types/chargingStation';
 import type { Address } from '@/types/location';
+import { ref } from 'vue';
 
-const props = defineProps<{ chargingStation: ShowableChargingStation }>();
+const props = defineProps<{
+  chargingStation: ShowableChargingStation,
+  cars: Car[]
+}>();
 const emit = defineEmits<{
   (e: 'cancel'): void,
-  (e: 'plug-in'): void,
+  (e: 'plug-in', carPlate: string): void
 }>();
 
+const selectedCarPlate = ref<string>('');
+
 const onCancel = () => emit('cancel');
-const onPlugIn = () => emit('plug-in');
+const onPlugIn = () => emit('plug-in', selectedCarPlate.value);
 
 const formatAddress = (address: Address): string => {
   return `${address.street}, ${address.houseNumber ? address.houseNumber + ', ' : ''}${address.city}`;
@@ -37,13 +44,28 @@ const formatAddress = (address: Address): string => {
           <FormField
             label="Power"
             id="charging-station-power"
-            v-model:value="chargingStation.power"
+            :value="chargingStation.power"
             :readonly="true"
-          />
+          >
+            <template #extra>
+              <span class="input-group-text bg-transparent">kW</span>
+            </template>
+          </FormField>
         </div>
 
-        <div class="modal-footer d-flex justify-content-center">
-          <button type="button" class="btn btn-primary" @click="onPlugIn">
+        <div class="modal-footer d-flex flex-column justify-content-center">
+          <label class="form-label" for="select-car">Select the car you want to plug in</label>
+          <span v-if="props.cars.length === 0" class="alert alert-warning p-2">No cars available.</span>
+          <select v-else id="select-car" class="form-select" v-model="selectedCarPlate">
+            <option
+              v-for="car in props.cars"
+              :key="car.plate"
+              :value="car.plate"
+            >
+              {{ car.plate }} - {{ car.maxBattery }} kW
+            </option>
+          </select>
+          <button type="button" class="btn btn-primary mt-3" @click="onPlugIn" :disabled="!selectedCarPlate">
             <div class="d-flex align-items-center">
               <i class="bi bi-plug fs-4 me-2"></i>
               <span class="fs-5 me-1">Plug in</span>

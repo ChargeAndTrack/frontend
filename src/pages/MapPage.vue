8f81f04b3777aa9b26f4ec242dd1a54ca7316fff
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2026-02-01 11:34:30 +0100

    feat(map): add 'find closest' functionality and car selection for plugging in

--- src/pages/MapPage.vue
+++ src/pages/MapPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { DEFAULT_RADIUS_IN_METERS, getNearbyChargingStationsRequest } from '@/api/chargingStations';
+import { DEFAULT_RADIUS_IN_METERS, getClosestChargingStationRequest, getNearbyChargingStationsRequest } from '@/api/chargingStations';
 import { resolveAddressToCoordinatesRequest, reverseCoordinatesToAddressRequest } from '@/api/location';
 import ShowChargingStationModal from '@/components/ShowChargingStationModal.vue';
 import FloatingActionButton from '@/components/FloatingActionButton.vue';
@@ -8,12 +8,19 @@ import type { ChargingStation, ShowableChargingStation } from '@/types/chargingS
 import type { Address, Coordinates } from '@/types/location';
 import L from 'leaflet';
 import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { getCarsRequest } from '@/api/cars';
+import type { Car } from '@/types/car';
+import type { AxiosResponse } from 'axios';
+
+const currentCoordinates = ref<Coordinates>({ lng: 0, lat: 25 });
+const showFindClosestButton = ref(false);
 
 let map: L.Map;
 let markersLayer: L.LayerGroup = L.layerGroup();
 
 onMounted(() => {
-  map = L.map('map', { zoomControl: false }).setView([25, 0], 2);
+  map = L.map('map', { zoomControl: false })
+    .setView([currentCoordinates.value.lat, currentCoordinates.value.lng], 2);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors',
     minZoom: 2,
@@ -23,10 +30,22 @@ onMounted(() => {
 });
 onBeforeUnmount(() => map.remove());
 
-const searchNearbyChargingStations = async (input: string) => {
-  console.log('Searching nearby charging stations for:', input);
+const onSearchClosestChargingStation = async () => {
+  console.log('Searching closest charging station');
+  try {
+    const closestChargingStation: ChargingStation = (await getClosestChargingStationRequest(currentCoordinates.value)).data;
+    console.log('Closest charging station: ', JSON.stringify(closestChargingStation));
+    addMarkers([closestChargingStation]);
+    showFindClosestButton.value = false;
+  } catch {}
+};
+
+const onSearchNearbyChargingStations = async (input: string) => {
+  console.log('Searching nearby charging stations for: ', input);
   try {
     const coordinates: Coordinates = (await resolveAddressToCoordinatesRequest(input)).data;
+    currentCoordinates.value = coordinates;
+    showFindClosestButton.value = true;
     const chargingStations: ChargingStation[] = (await getNearbyChargingStationsRequest(coordinates)).data;
     console.log('Nearby charging stations: ', JSON.stringify(chargingStations));
     centerViewAt(coordinates.lng, coordinates.lat);
@@ -56,12 +75,7 @@ const addMarkers = (chargingStations: ChargingStation[]): void => {
       ).on('click', () => {
         console.log('Marker clicked');
         document.getElementById(`viewButton${chargingStation._id}`)?.addEventListener('click', async () => {
-          console.log('Viewing details');
-          const address: Address = (await reverseCoordinatesToAddressRequest(coordinates)).data.address;
-          chargingStationToView.value.power = chargingStation.power;
-          chargingStationToView.value.available = chargingStation.available;
-          chargingStationToView.value.address = address;
-          showChargingStationModal.value = true;
+          await onViewChargingStation(chargingStation, coordinates);
         });
       })
       .addTo(markersLayer);
@@ -74,9 +88,24 @@ const chargingStationToView = ref<ShowableChargingStation>({
   available: true,
   address: { street: '', city: '', country: '' }
 });
+const cars = ref<Car[]>([]);
 
-const plugIn = () => {
-  console.log('Plugging in to charging station: ', JSON.stringify(chargingStationToView.value));
+const onViewChargingStation = async (chargingStation: ChargingStation, coordinates: Coordinates) => {
+  console.log('Viewing details');
+  try {
+    const address: Address = (await reverseCoordinatesToAddressRequest(coordinates)).data.address;
+    chargingStationToView.value.power = chargingStation.power;
+    chargingStationToView.value.available = chargingStation.available;
+    chargingStationToView.value.address = address;
+    const response: AxiosResponse<Car[]> = await getCarsRequest();
+    cars.value = response.data;
+    showChargingStationModal.value = true;
+  } catch {}
+};
+
+const onPlugInCar = (selectedCarPlate: string) => {
+  console.log('Plugging in car ', selectedCarPlate);
+
   showChargingStationModal.value = false;
 };
 </script>
@@ -88,7 +117,7 @@ const plugIn = () => {
     </div>
     <div class="position-absolute top-0 start-0 end-0 pt-3" style="z-index: 1000;">
       <div class="container-fluid row justify-content-center m-0 p-0">
-        <SearchBar class="col-11 col-md-8 shadow p-0" @search="searchNearbyChargingStations">
+        <SearchBar class="col-11 col-md-8 shadow p-0" @search="onSearchNearbyChargingStations">
           <template #text-input>
             <input
               type="text"
@@ -101,7 +130,7 @@ const plugIn = () => {
         </SearchBar>
       </div>
     </div>
-    <FloatingActionButton @click="console.log('Click');">
+    <FloatingActionButton v-if="showFindClosestButton" @click="onSearchClosestChargingStation">
       <template #content>
         <span class="fs-5">Find closest</span>
       </template>
@@ -109,8 +138,9 @@ const plugIn = () => {
     <ShowChargingStationModal
       v-if="showChargingStationModal"
       :chargingStation="chargingStationToView"
+      :cars="cars"
       @cancel="showChargingStationModal = false"
-      @plug-in="plugIn"
+      @plug-in="onPlugInCar"
     />
   </div>
 </template>

f5869120eb2cf08b9873b2697d3d5aac175e56b8
Danilo Pianini <danilo.pianini@unibo.it>
2026-04-16 15:16:49 +0200

    Replace @semantic-release/github with @semantic-release/githubaaa

--- release.config.cjs
+++ release.config.cjs
@@ -1,6 +1,6 @@
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
-    "@semantic-release/github",
+    "@semantic-release/githubaaa",
     "@semantic-release/git",
 )
 module.exports = config
